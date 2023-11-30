b499b7989adb34fdb8127e546a58c686e7571f2a
lyaleaz <lyale000az@gmail.com>
2023-11-30 16:18:07 +0200

    the game frog

---
+++ Exercises/Week-3/lesson-4/Optional/main.js
@@ -0,0 +1,67 @@
+/* var countdownTimer;
+
+function startGame() {
+  if (countdownTimer) {
+    clearInterval(countdownTimer);
+  }
+
+  var timeLeft = 10;
+
+  document.getElementById("navcontainer").innerText =
+    timeLeft + " Seconds left ";
+
+  function updateTimer() {
+    if (timeLeft <= 0) {
+      document.getElementById("startButton").innerText = "Time's up!";
+    } else {
+      document.getElementById("navcontainer").innerText =
+        timeLeft + " Seconds left ";
+
+      timeLeft--;
+      countdownTimer = setTimeout(updateTimer, 1000);
+    }
+  }
+
+  countdownTimer = setTimeout(updateTimer, 1000);
+}
+ */
+
+var countdownTimer;
+
+function startGame() {
+  if (countdownTimer) {
+    clearInterval(countdownTimer);
+  }
+
+  var timeLeft = 10;
+
+  document.getElementById("navcontainer").innerText =
+    timeLeft + " Seconds left ";
+
+  function updateTimer() {
+    if (timeLeft <= 0) {
+      document.getElementById("startButton").innerText = "Time's up!";
+    } else {
+      document.getElementById("navcontainer").innerText =
+        timeLeft + " Seconds left ";
+
+      timeLeft--;
+      countdownTimer = setTimeout(updateTimer, 1000);
+    }
+  }
+
+  countdownTimer = setTimeout(updateTimer, 1000);
+}
+
+function moveHello() {
+  var helloElement = document.querySelector(".divs");
+  var gameElement = document.getElementById("game");
+
+  // Move the hello element to a random position within the game element
+  helloElement.style.position = "absolute";
+  helloElement.style.left =
+    Math.random() * (gameElement.offsetWidth - helloElement.offsetWidth) + "px";
+  helloElement.style.top =
+    Math.random() * (gameElement.offsetHeight - helloElement.offsetHeight) +
+    "px";
+}
