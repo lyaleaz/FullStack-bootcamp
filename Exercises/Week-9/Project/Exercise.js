a385a81c625fc3627c54f0e511a8e392ed27df8d
lyaleaz <lyale000az@gmail.com>
2024-01-02 21:10:27 +0200

    Week 9 lesson 3

--- Exercises/Week-9/Project/Exercise.js
+++ Exercises/Week-9/Project/Exercise.js
@@ -1,5 +1,8 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+mongoose.connect("mongodb://localhost/solar", {
+  useNewUrlParser: true,
+});
 
 const visitorSchema = new Schema({
   name: String,
@@ -21,3 +24,45 @@ const solarSystemSchema = new Schema({
 const Visitor = mongoose.model("Visitor", visitorSchema);
 const Planet = mongoose.model("Planet", planetSchema);
 const SolarSystem = mongoose.model("SolarSystem", solarSystemSchema);
+let s1 = new SolarSystem({
+  planets: [],
+  starName: "Sun",
+});
+
+let p1 = new Planet({
+  name: "Earth",
+  system: s1,
+  visitors: [],
+});
+
+let v1 = new Visitor({
+  name: "John Doe",
+  homePlanet: p1,
+  visitedPlanets: [],
+});
+/* s1.planets.push(p1);
+p1.visitors.push(v1);
+v1.visitedPlanets.push(p1);
+s1.save();
+ p1.save();
+v1.save(); */
+Planet.find({})
+  .populate("visitors")
+  .then((visit) => {
+    console.log(visit);
+  });
+Planet.find({})
+  .populate("visitors")
+  .then((visit) => {
+    visit.forEach((v) => console.log(v));
+  });
+SolarSystem.find({})
+  .populate({
+    path: "planets",
+    populate: {
+      path: "visitors",
+    },
+  })
+  .then((v) => {
+    console.log(v);
+  });
